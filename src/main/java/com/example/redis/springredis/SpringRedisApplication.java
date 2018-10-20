93302acfdc549b945549c977cba13376ea042b55
Levindo Neto <levindogtn@gmail.com>
2018-10-20 17:07:03 -0300

    Add beans for redis configuration

--- src/main/java/com/example/redis/springredis/SpringRedisApplication.java
+++ src/main/java/com/example/redis/springredis/SpringRedisApplication.java
@@ -1,12 +1,30 @@
 package com.example.redis.springredis;
 
+import com.example.redis.springredis.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class SpringRedisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRedisApplication.class, args);
-	}
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        // Get the connection factory in build
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate<String, Student> redisTemplate() {
+        RedisTemplate<String, Student> redisTemplate =
+            new RedisTemplate<String, Student>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return null;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisApplication.class, args);
+    }
 }
